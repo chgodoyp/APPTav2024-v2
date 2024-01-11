cac94dce835d6c453345e06741f1d55f73b0aa1e
godoyp <Ch.godoyp@duocuc.cl>
2024-01-11 19:53:08 -0300

    home

--- APPTav2024-v2/src/app/app.component.ts
+++ APPTav2024-v2/src/app/app.component.ts
@@ -14,8 +14,8 @@ export class AppComponent {
   public appPages = [
     { title: 'Inicio', url: '/welcome', icon: 'home' },
     { title: 'Perfil', url: '/profile', icon: 'person' },
-    { title: 'Retornar a mi hogar', url:'/return-trip', icon: 'car'},
-    { title: 'Nosotros', url: '/about', icon: 'people' },
+    //{ title: 'Retornar a mi hogar', url:'/return-trip', icon: 'car'},
+    //{ title: 'Nosotros', url: '/about', icon: 'people' },
   ];
 
   username: string;
